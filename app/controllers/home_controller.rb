d2bf4dded4775376c818ba620b5c4600b03efe3f
Ruchi <ruchi@jeronone.com>
2014-05-27 15:05:25 +0530

    changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -16,7 +16,7 @@ class HomeController < ApplicationController
   end
   def upload
   uploaded_io = params[:datafile]
-  File.open(Rails.root.join('public', uploaded_io.original_filename), 'wb') do |file|
+  File.open(Rails.root.join('public', 'upload', uploaded_io.original_filename), 'wb') do |file|
   file.write(uploaded_io.read)
   end
   
