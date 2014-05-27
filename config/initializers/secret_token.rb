19685838c193f25c2dca9300cd206b19c832580e
Ruchi <ruchi@jeronone.com>
2014-05-27 10:28:41 +0530

    changes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vesriontest::Application.config.secret_key_base = '5c3cc2dd44fa14d28465054219b29c1110ca4169a9a6a24f9fa919b5a09e170f8fb35a1861e383bb322a120dca0eea0b0d61dd3693cd68aab048b2b777cee3c2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Vesriontest::Application.config.session_store :cookie_store, key: '_vesriontest_session'
