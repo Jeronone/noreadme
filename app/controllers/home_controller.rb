853cde0d4f39565ea8ba075862ea6f9775569e6b
Ruchi <ruchi@jeronone.com>
2014-05-30 17:04:13 +0530

    changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -15,16 +15,11 @@ class HomeController < ApplicationController
     #@orders   = ShopifyAPI::Order.find(:all, :params => {:limit => 5, :order => "created_at DESC" })
   end
   def upload
-  if @params[:image_id].present?
-     #preloaded = Cloudinary::PreloadedFile.new(params[:image_id])
-     # Verify signature by calling preloaded.valid?
-     #@model.image_id = preloaded.identifier
-	  #flash[:notice] = "Successfully updated!"
-  end
   
-      #@product = ShopifyAPI::Product.find(params[:id])
-	  # variantid=params[:vid]
-	  # varianttitle=params[:vtitle]
+  
+      @product = ShopifyAPI::Product.find(params[:id])
+	   variantid=params[:vid]
+	   varianttitle=params[:vtitle]
 	  # code to check if the image already exist or not
 	  # if yes then delete the existing one 
 	 
@@ -32,15 +27,15 @@ class HomeController < ApplicationController
 	 
 	 # puts YAML::dump(variantid)
 	 # puts YAML::dump(varianttitle)
-    #if(@product)
-      #  uploaded_image = params[:product][:product_image]
-      #  @product.images << ShopifyAPI::Image.new({:attachment =>
-#Base64.encode64(uploaded_image.read), :filename => varianttitle+"-"+variantid+".png"})
- #@product.save
+    if(@product)
+        uploaded_image = params[:product][:product_image]
+        @product.images << ShopifyAPI::Image.new({:attachment =>
+Base64.encode64(uploaded_image.read), :filename => varianttitle+"-"+variantid+".png"})
+ @product.save
 		#product.update_attributes(params[:product])
-        #flash[:notice] = "Successfully updated!"
+        flash[:notice] = "Successfully updated!"
        
-    #end
+    end
 	  end
   
 end
