6184c1180211f3c7431b38ec99faa82f1d7a62bc
Ruchi <ruchi@jeronone.com>
2014-06-02 18:08:11 +0530

    g

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -61,7 +61,7 @@ Base64.encode64(uploaded_image.read), :filename => varianttitle+"-"+variantid+".
 	def destroy
 	@product=ShopifyAPI::Product.find(params[:id])
 	@img=params[:imgid]
-	image = ShopifyAPI::Image.find(params[:imgid], params[:id])
+	image = ShopifyAPI::Image.find(params[:imgid])
 	image.destroy
 	end
   
