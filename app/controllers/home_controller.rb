b2d0e8c32b64795f349ba3d617bc53b263146443
Ruchi <ruchi@jeronone.com>
2014-06-02 18:24:57 +0530

    g

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -62,7 +62,7 @@ Base64.encode64(uploaded_image.read), :filename => varianttitle+"-"+variantid+".
 	@product=ShopifyAPI::Product.find(params[:id])
 	img=params[:imgid]
 	pid=params[:id]
-	image = ShopifyAPI::Image.find(params[:imgid], :params => {:product_id => 273463821})
+	image = ShopifyAPI::Image.find(params[:imgid], :params => {:product_id => params[:id]})
 	#image=ShopifyAPI::Image.find(1, :params => {:product_id => @pid})
 	#image = ShopifyAPI::Image.find(params[:imgid], params[:id])
 	image.destroy
