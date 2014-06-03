14b4e9a72e8a34e867117da899dba40e1353ba88
Ruchi <ruchi@jeronone.com>
2014-06-03 10:09:34 +0530

    changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -38,7 +38,7 @@ class HomeController < ApplicationController
     if(@product1)
         uploaded_image = params[:product][:product_image]
         @product1.images << ShopifyAPI::Image.new({:attachment =>
-Base64.encode64(uploaded_image.read), :filename => varianttitle+"-"+variantid+".png",:metafield =>{:key => "alt",:value=>"ruchi",:value_type=>"string",:namespace => "tags"}})
+Base64.encode64(uploaded_image.read), :filename =>variantid+".png",:metafield =>{:key => "alt",:value=>"ruchi",:value_type=>"string",:namespace => "tags"}})
  @product1.save
 		#product.update_attributes(params[:product])
         flash[:notice] = "Successfully updated!"
