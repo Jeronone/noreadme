df383f654b931431f4f129272839d94d735a5577
Ruchi <ruchi@jeronone.com>
2014-06-04 16:19:51 +0530

    changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -120,7 +120,7 @@ format.html { redirect_to :action => 'product', :id => @product1.id }
 	   @selectedColor=params[:car][:color]
 	   @selectedColor=@selectedColor.gsub(/#/, "")
 	   @select= "http://placehold.it/50x50/#{@selectedColor}/ffffff&text=+"
-	   @p=params[:product_id]
+	   #@p=params[:product_id]
 	   @variant=[:vid]
 	   #@product = ShopifyAPI::Product.find(params[:product_id])
 	   Cloudinary::Uploader.upload(@select,:public_id =>@variant)
