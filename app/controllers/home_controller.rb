f0cd0fbf34c9cebb5c5f63bf3f35f26d71040bbd
Ruchi <ruchi@jeronone.com>
2014-06-05 15:22:01 +0530

    chnages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -81,9 +81,9 @@ format.html { redirect_to :action => 'product', :id => @product1.id }
 	@vid=params[:vid]
 	pid=params[:pid]
 	@vtitle=params[:vtitle]
-	@vtitle=@vtitle.to_s() +'-'
-	@vtitle.concat(@vid.to_s)
-	Cloudinary::Uploader.destroy(@vtid, :invalidate => true)
+	@vtitle=pid.to_s() +'-'
+	@vtitle.concat(vtid.to_s)
+	Cloudinary::Uploader.destroy(@vtitle, :invalidate => true)
 	format.html { redirect_to :action => 'product', :id => pid }
 	end
 
