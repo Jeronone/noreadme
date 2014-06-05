7a239c1562d0e9e2ffbefb8cfc0f83c0b8cbf2b3
Ruchi <ruchi@jeronone.com>
2014-06-05 18:31:20 +0530

    chnages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -34,7 +34,7 @@ class HomeController < ApplicationController
 			@case="case123"
 		 end
 	   else
-	     if params.has_key?(:oid) && params[:oid2].empty?
+	     if params.has_key?(:oid1) && params[:oid2].empty?
 			@oidf=@oid
 			@case="case1"
 			if params.has_key?(:oid3)
