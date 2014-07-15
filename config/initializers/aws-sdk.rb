1d1b30fd8e51a83fc438c415e2f5ded8df2fb0f4
Ruchi <ruchi@jeronone.com>
2014-07-15 11:51:21 +0530

    changes

--- config/initializers/aws-sdk.rb
+++ config/initializers/aws-sdk.rb
@@ -1,4 +1,3 @@
-AWS.config(access_key_id:     ENV['AKIAI4AKW43PXCSKQFHA'],
-           secret_access_key: ENV['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'] )
-
+AWS.config(access_key_id: ENV['AKIAI4AKW43PXCSKQFHA'],
+           secret_access_key: ENV['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'])
 S3_BUCKET = AWS::S3.new.buckets[ENV['BUCKET15-7-14']]
